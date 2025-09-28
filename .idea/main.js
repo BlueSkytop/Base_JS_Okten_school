3a7fcba998bdff2ac9e5cf209d1561dcb5fa337c
BlueSkytop <olgakutnja@gmail.com>
2025-09-28 23:39:39 +0300

    init commit

---
+++ .idea/main.js
@@ -0,0 +1,30 @@
+//#dYQNrBV
+//Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+  //  Вивести кожну змінну за допомогою: console.log
+
+let hello = 'hello';
+console.log(hello);
+let owu = 'owu';
+console.log(owu);
+let com = 'com';
+console.log(com);
+let ua = 'ua';
+console.log(ua);
+let number1 = 1;
+console.log(1);
+let number2 = 10;
+console.log(10);
+let number3 = -999;
+console.log(-999);
+let number4 = 123;
+console.log(123);
+let number5 = 3.14;
+console.log(3.14);
+let number6 = 2.7;
+console.log(2.7);
+let number7 = 16;
+console.log(16);
+let boolen = true;
+console.log(true);
+let boolen2 = false;
+console.log(false);
